d3fc515ee9236c703998992689a35c94c573b9f0
213am <dev.213am@gmail.com>
2025-01-24 09:35:24 +0900

    [chore] recharts 패키지 추가

--- src/components/useModal.jsx
+++ src/components/useModal.jsx
@@ -1,9 +1,6 @@
 import React, { useCallback, useState } from "react";
 import Modal from "../components/Modal";
-<<<<<<< HEAD
 
-=======
->>>>>>> 0c551d9011acde111c6a1db876557c097c78580a
 const useModal = ({ useBlur = true, title } = {}) => {
   const [isOpen, setIsOpen] = useState(false);
   const open = useCallback(() => {
